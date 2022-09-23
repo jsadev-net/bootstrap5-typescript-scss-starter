9b3c0328bf4346803aa3a61bac260bf38a08666f
Jerome Sadikovitsch <jerome.sadikovitsch@logobject.ch>
2022-09-23 10:14:36 +0200

    upgrade dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const ts = require('gulp-typescript');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify-es').default;
